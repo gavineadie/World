ad312b9af1105eef2cdf032ee0528cd00e28662c
Gavin Eadie <gavin@umich.edu>
2024-01-20 22:23:01 -0500

    Initial Commit

---
+++ World/WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldApp.swift
+//  World
+//
+//  Created by Gavin Eadie on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
